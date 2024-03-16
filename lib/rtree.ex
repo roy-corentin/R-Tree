54a548b00f9c249ed3cc19e7eb3c346ee0d683be
roy-corentin <corentin.roy02@laposte.net>
2024-03-16 15:40:34 +0100

    [ADD] mix new rtree

---
+++ lib/rtree.ex
@@ -0,0 +1,18 @@
+defmodule Rtree do
+  @moduledoc """
+  Documentation for `Rtree`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rtree.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rtree_test.exs
@@ -0,0 +1,8 @@
+defmodule RtreeTest do
+  use ExUnit.Case
+  doctest Rtree
+
+  test "greets the world" do
+    assert Rtree.hello() == :world
+  end
+end
