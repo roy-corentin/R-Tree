16ef64be07dc42006ee8871632f224f26d2aaca2
roy-corentin <corentin.roy02@laposte.net>
2024-03-16 17:57:13 +0100

    [ADD] different modules and basic insert
    Insert method only push in objects and update bounding box

--- lib/rtree.ex
+++ lib/rtree.ex
@@ -1,18 +1,47 @@
-defmodule Rtree do
-  @moduledoc """
-  Documentation for `Rtree`.
-  """
+defmodule RBoundingBox do
+  defstruct min_x: 0, min_y: 0, max_x: 0, max_y: 0
 
-  @doc """
-  Hello world.
+  def update(_box = nil, _point = %{x: x, y: y}) do
+    %RBoundingBox{min_x: x - 1, min_y: y - 1, max_x: x + 1, max_y: y + 1}
+  end
 
-  ## Examples
-
-      iex> Rtree.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def update(box = %RBoundingBox{}, _point = %{x: x, y: y}) do
+    %RBoundingBox{
+      min_x: min(box.min_x, x - 1),
+      min_y: min(box.min_y, y - 1),
+      max_x: max(box.max_x, x + 1),
+      max_y: max(box.max_y, y + 1)
+    }
   end
 end
+
+defmodule RObject do
+  defstruct [:x, :y, :data]
+end
+
+defmodule RNode do
+  @type bounding :: %RBoundingBox{}
+  @type objects :: list(%RObject{})
+  defstruct bounding: nil, children: [], objects: []
+end
+
+defmodule RTree do
+  def insert(node = %RNode{}, object = %RObject{}) do
+    %RNode{
+      node
+      | bounding: RBoundingBox.update(node.bounding, object),
+        objects: node.objects ++ [object]
+    }
+  end
+end
+
+defmodule Main do
+  def main do
+    %RNode{}
+    |> RTree.insert(%RObject{x: 1, y: 2, data: "data"})
+    |> RTree.insert(%RObject{x: 2, y: 3, data: "data"})
+    |> IO.inspect()
+  end
+end
+
+Main.main()
