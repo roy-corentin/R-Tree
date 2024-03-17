428f251737cf32fcae084d3cde0c114ecaccf680
roy-corentin <corentin.roy02@laposte.net>
2024-03-17 15:37:15 +0100

    [UP] split insert in the child with the less bounding increase

--- lib/rtree.ex
+++ lib/rtree.ex
@@ -19,8 +19,24 @@ defmodule RBoundingBox do
     }
   end
 
+  @spec contains(%RBoundingBox{}, %{x: integer, y: integer}) :: boolean
   def contains(box = %RBoundingBox{}, %{x: x, y: y}) do
     x >= box.min_x and x <= box.max_x and y >= box.min_y and y <= box.max_y
+  end
+
+  @spec increases(%{x: integer, y: integer}, %RBoundingBox{}, %RBoundingBox{}) :: list(integer)
+  def increases(%{x: x, y: y}, box1 = %RBoundingBox{}, box2 = %RBoundingBox{}) do
+    [increase(box1, %{x: x, y: y}), increase(box2, %{x: x, y: y})]
+  end
+
+  @spec increase(%RBoundingBox{}, %{x: integer, y: integer}) :: integer
+  def increase(box = %RBoundingBox{}, %{x: x, y: y}) do
+    area(update(box, %{x: x, y: y})) - area(box)
+  end
+
+  @spec area(%RBoundingBox{}) :: integer
+  def area(box = %RBoundingBox{}) do
+    (box.max_x - box.min_x) * (box.max_y - box.min_y)
   end
 end
 
@@ -46,22 +62,34 @@ defmodule RTree do
     }
 
     if length(node.objects) > @m do
-      split_leaf(node.objects, nil, nil)
+      %RNode{node | objects: [], children: split_leaf(node.objects, nil, nil)}
     else
       node
     end
   end
 
-  def insert(node = %RNode{children: children}, object = %RObject{}) when length(children) > 0 do
-    if RBoundingBox.contains(node.bounding, object) do
-      %RNode{node | children: Enum.map(node.children, fn child -> insert(child, object) end)}
+  @spec insert(%RNode{}, %RObject{}) :: %RNode{}
+  def insert(node = %RNode{children: children}, object = %RObject{}) when length(children) == 2 do
+    {increase1, increase2} =
+      RBoundingBox.increases(object, children[0].bounding, children[1].bounding)
+
+    if increase1 < increase2 do
+      %RNode{
+        node
+        | bounding: RBoundingBox.update(node.bounding, object),
+          children: [RTree.insert(children[0], object), children[1]]
+      }
     else
-      node
+      %RNode{
+        node
+        | bounding: RBoundingBox.update(node.bounding, object),
+          children: [children[0], RTree.insert(children[1], object)]
+      }
     end
   end
 
   defp split_leaf([], leaf_left, leaf_right) do
-    %RNode{children: [leaf_left, leaf_right]}
+    [leaf_left, leaf_right]
   end
 
   defp split_leaf([object | rest], nil, nil) do
@@ -73,8 +101,10 @@ defmodule RTree do
   end
 
   defp split_leaf([object | rest], leaf_left, leaf_right) do
-    if RBoundingBox.update(leaf_left.bounding, object) <
-         RBoundingBox.update(leaf_right.bounding, object) do
+    [increase1, increase2] =
+      RBoundingBox.increases(object, leaf_left.bounding, leaf_right.bounding)
+
+    if increase1 < increase2 do
       split_leaf(rest, RTree.insert(leaf_left, object), leaf_right)
     else
       split_leaf(rest, leaf_left, RTree.insert(leaf_right, object))
@@ -86,8 +116,8 @@ defmodule Main do
   def main do
     %RNode{}
     |> RTree.insert(%RObject{x: 1, y: 2, data: "data"})
-    |> RTree.insert(%RObject{x: 2, y: 3, data: "data"})
-    |> RTree.insert(%RObject{x: 3, y: 4, data: "data"})
+    |> RTree.insert(%RObject{x: 20, y: 30, data: "data"})
+    |> RTree.insert(%RObject{x: 15, y: 4, data: "data"})
     |> IO.inspect()
   end
 end
