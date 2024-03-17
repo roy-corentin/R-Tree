eed6e32033a18dd142462df473f1b3d7f1e53430
roy-corentin <corentin.roy02@laposte.net>
2024-03-17 15:50:29 +0100

    [ADD] some improvement
    Notable improvement is that area is compute and store

--- lib/rtree.ex
+++ lib/rtree.ex
@@ -3,19 +3,27 @@ defmodule RBoundingBox do
   @type min_y :: integer
   @type max_x :: integer
   @type max_y :: integer
-  defstruct min_x: 0, min_y: 0, max_x: 0, max_y: 0
+  @type area :: integer
+  defstruct min_x: 0, min_y: 0, max_x: 0, max_y: 0, area: 0
 
   @spec update(nil, %{x: integer, y: integer}) :: %RBoundingBox{}
   def update(nil, %{x: x, y: y}) do
-    %RBoundingBox{min_x: x, min_y: y, max_x: x, max_y: y}
+    %RBoundingBox{min_x: x, min_y: y, max_x: x, max_y: y, area: 0}
   end
 
   def update(box = %RBoundingBox{}, %{x: x, y: y}) do
+    new_min_x = min(box.min_x, x - 1)
+    new_min_y = min(box.min_y, y - 1)
+    new_max_x = max(box.max_x, x + 1)
+    new_max_y = max(box.max_y, y + 1)
+    area = (new_max_x - new_min_x) * (new_max_y - new_min_y)
+
     %RBoundingBox{
-      min_x: min(box.min_x, x - 1),
-      min_y: min(box.min_y, y - 1),
-      max_x: max(box.max_x, x + 1),
-      max_y: max(box.max_y, y + 1)
+      min_x: new_min_x,
+      min_y: new_min_y,
+      max_x: new_max_x,
+      max_y: new_max_y,
+      area: area
     }
   end
 
@@ -24,19 +32,10 @@ defmodule RBoundingBox do
     x >= box.min_x and x <= box.max_x and y >= box.min_y and y <= box.max_y
   end
 
-  @spec increases(%{x: integer, y: integer}, %RBoundingBox{}, %RBoundingBox{}) :: list(integer)
-  def increases(%{x: x, y: y}, box1 = %RBoundingBox{}, box2 = %RBoundingBox{}) do
-    [increase(box1, %{x: x, y: y}), increase(box2, %{x: x, y: y})]
-  end
-
-  @spec increase(%RBoundingBox{}, %{x: integer, y: integer}) :: integer
-  def increase(box = %RBoundingBox{}, %{x: x, y: y}) do
-    area(update(box, %{x: x, y: y})) - area(box)
-  end
-
-  @spec area(%RBoundingBox{}) :: integer
-  def area(box = %RBoundingBox{}) do
-    (box.max_x - box.min_x) * (box.max_y - box.min_y)
+  def increases(object, box1 = %RBoundingBox{}, box2 = %RBoundingBox{}) do
+    increase1 = update(box1, object).area - box1.area
+    increase2 = update(box2, object).area - box2.area
+    [increase1, increase2]
   end
 end
 
@@ -77,13 +76,13 @@ defmodule RTree do
       %RNode{
         node
         | bounding: RBoundingBox.update(node.bounding, object),
-          children: [RTree.insert(children[0], object), children[1]]
+          children: [insert(children[0], object), children[1]]
       }
     else
       %RNode{
         node
         | bounding: RBoundingBox.update(node.bounding, object),
-          children: [children[0], RTree.insert(children[1], object)]
+          children: [children[0], insert(children[1], object)]
       }
     end
   end
@@ -93,11 +92,11 @@ defmodule RTree do
   end
 
   defp split_leaf([object | rest], nil, nil) do
-    split_leaf(rest, RTree.insert(%RNode{}, object), nil)
+    split_leaf(rest, insert(%RNode{}, object), nil)
   end
 
   defp split_leaf([object | rest], leaf_left, nil) do
-    split_leaf(rest, leaf_left, RTree.insert(%RNode{}, object))
+    split_leaf(rest, leaf_left, insert(%RNode{}, object))
   end
 
   defp split_leaf([object | rest], leaf_left, leaf_right) do
@@ -105,9 +104,9 @@ defmodule RTree do
       RBoundingBox.increases(object, leaf_left.bounding, leaf_right.bounding)
 
     if increase1 < increase2 do
-      split_leaf(rest, RTree.insert(leaf_left, object), leaf_right)
+      split_leaf(rest, insert(leaf_left, object), leaf_right)
     else
-      split_leaf(rest, leaf_left, RTree.insert(leaf_right, object))
+      split_leaf(rest, leaf_left, insert(leaf_right, object))
     end
   end
 end
