0bd5ffcad7d285d852d81656828fa9d6a7711481
roy-corentin <corentin.roy02@laposte.net>
2024-03-17 19:47:02 +0100

    [UP] improve rtree + add test

--- lib/rtree.ex
+++ lib/rtree.ex
@@ -6,19 +6,36 @@ defmodule RBoundingBox do
   @type area :: integer
   defstruct min_x: 0, min_y: 0, max_x: 0, max_y: 0, area: 0
 
-  @spec update(nil, %{x: integer, y: integer}) :: %RBoundingBox{}
-  def update(nil, %{x: x, y: y}) do
-    %RBoundingBox{min_x: x, min_y: y, max_x: x, max_y: y, area: 0}
+  @spec create(%{x: integer, y: integer}, %{x: integer, y: integer}) :: %RBoundingBox{}
+  def create(%{x: x1, y: y1}, %{x: x2, y: y2}) do
+    min_x = min(x1, x2)
+    min_y = min(y1, y2)
+    max_x = max(x1, x2)
+    max_y = max(y1, y2)
+    area = (max_x - min_x) * (max_y - min_y)
+
+    %__MODULE__{
+      min_x: min_x,
+      min_y: min_y,
+      max_x: max_x,
+      max_y: max_y,
+      area: area
+    }
   end
 
-  def update(box = %RBoundingBox{}, %{x: x, y: y}) do
-    new_min_x = min(box.min_x, x - 1)
-    new_min_y = min(box.min_y, y - 1)
-    new_max_x = max(box.max_x, x + 1)
-    new_max_y = max(box.max_y, y + 1)
+  @spec update(nil, %{x: integer, y: integer}) :: %__MODULE__{}
+  def update(nil, %{x: x, y: y}) do
+    %__MODULE__{min_x: x, min_y: y, max_x: x, max_y: y, area: 0}
+  end
+
+  def update(box = %__MODULE__{}, %{x: x, y: y}) do
+    new_min_x = min(box.min_x, x)
+    new_min_y = min(box.min_y, y)
+    new_max_x = max(box.max_x, x)
+    new_max_y = max(box.max_y, y)
     area = (new_max_x - new_min_x) * (new_max_y - new_min_y)
 
-    %RBoundingBox{
+    %__MODULE__{
       min_x: new_min_x,
       min_y: new_min_y,
       max_x: new_max_x,
@@ -27,15 +44,10 @@ defmodule RBoundingBox do
     }
   end
 
-  @spec contains(%RBoundingBox{}, %{x: integer, y: integer}) :: boolean
-  def contains(box = %RBoundingBox{}, %{x: x, y: y}) do
-    x >= box.min_x and x <= box.max_x and y >= box.min_y and y <= box.max_y
-  end
-
-  def increases(object, box1 = %RBoundingBox{}, box2 = %RBoundingBox{}) do
+  def increases(object, box1 = %__MODULE__{}, box2 = %__MODULE__{}) do
     increase1 = update(box1, object).area - box1.area
     increase2 = update(box2, object).area - box2.area
-    [increase1, increase2]
+    {increase1, increase2}
   end
 end
 
@@ -46,14 +58,20 @@ end
 defmodule RNode do
   @type bounding :: %RBoundingBox{}
   @type objects :: list(%RObject{})
-  defstruct bounding: nil, children: [], objects: []
+  @type children :: %{left: %RNode{}, right: %RNode{}}
+  defstruct bounding: nil, children: nil, objects: []
+
+  def create(%{objects: objects}) do
+    box = Enum.reduce(objects, nil, fn object, acc -> RBoundingBox.update(acc, object) end)
+    %__MODULE__{bounding: box, objects: objects}
+  end
 end
 
 defmodule RTree do
   @m 2
 
   @spec insert(%RNode{}, %RObject{}) :: %RNode{}
-  def insert(node = %RNode{}, object = %RObject{}) do
+  def insert(node = %RNode{children: nil}, object = %RObject{}) do
     node = %RNode{
       node
       | bounding: RBoundingBox.update(node.bounding, object),
@@ -61,52 +79,54 @@ defmodule RTree do
     }
 
     if length(node.objects) > @m do
-      %RNode{node | objects: [], children: split_leaf(node.objects, nil, nil)}
+      %RNode{node | objects: [], children: split(node.objects)}
     else
       node
     end
   end
 
   @spec insert(%RNode{}, %RObject{}) :: %RNode{}
-  def insert(node = %RNode{children: children}, object = %RObject{}) when length(children) == 2 do
+  def insert(node = %RNode{children: %{left: left, right: right}}, object = %RObject{}) do
     {increase1, increase2} =
-      RBoundingBox.increases(object, children[0].bounding, children[1].bounding)
+      RBoundingBox.increases(object, left.bounding, right.bounding)
 
     if increase1 < increase2 do
       %RNode{
         node
         | bounding: RBoundingBox.update(node.bounding, object),
-          children: [insert(children[0], object), children[1]]
+          children: %{left: insert(left, object), right: right}
       }
     else
       %RNode{
         node
         | bounding: RBoundingBox.update(node.bounding, object),
-          children: [children[0], insert(children[1], object)]
+          children: %{left: left, right: insert(right, object)}
       }
     end
   end
 
-  defp split_leaf([], leaf_left, leaf_right) do
-    [leaf_left, leaf_right]
+  defp split(objects) do
+    {left, right} = Enum.min_max_by(objects, fn object -> object.x + object.y end)
+
+    {left_leaf, right_leaf} =
+      {RNode.create(%{objects: [left]}), RNode.create(%{objects: [right]})}
+
+    (objects -- [left, right])
+    |> distribute(left_leaf, right_leaf)
   end
 
-  defp split_leaf([object | rest], nil, nil) do
-    split_leaf(rest, insert(%RNode{}, object), nil)
+  defp distribute([], leaf_left, leaf_right) do
+    %{left: leaf_left, right: leaf_right}
   end
 
-  defp split_leaf([object | rest], leaf_left, nil) do
-    split_leaf(rest, leaf_left, insert(%RNode{}, object))
-  end
-
-  defp split_leaf([object | rest], leaf_left, leaf_right) do
-    [increase1, increase2] =
+  defp distribute([object | rest], leaf_left, leaf_right) do
+    {left_increase, right_increase} =
       RBoundingBox.increases(object, leaf_left.bounding, leaf_right.bounding)
 
-    if increase1 < increase2 do
-      split_leaf(rest, insert(leaf_left, object), leaf_right)
+    if left_increase <= right_increase do
+      distribute(rest, insert(leaf_left, object), leaf_right)
     else
-      split_leaf(rest, leaf_left, insert(leaf_right, object))
+      distribute(rest, leaf_left, insert(leaf_right, object))
     end
   end
 end
@@ -116,7 +136,10 @@ defmodule Main do
     %RNode{}
     |> RTree.insert(%RObject{x: 1, y: 2, data: "data"})
     |> RTree.insert(%RObject{x: 20, y: 30, data: "data"})
-    |> RTree.insert(%RObject{x: 15, y: 4, data: "data"})
+    |> RTree.insert(%RObject{x: 15, y: 18, data: "data"})
+    |> RTree.insert(%RObject{x: 30, y: 10, data: "data"})
+    |> RTree.insert(%RObject{x: 1, y: 10, data: "data"})
+    |> RTree.insert(%RObject{x: 40, y: 7, data: "data"})
     |> IO.inspect()
   end
 end
--- test/rtree_test.exs
+++ test/rtree_test.exs
@@ -1,8 +1,46 @@
-defmodule RtreeTest do
+defmodule RTreeTest do
   use ExUnit.Case
-  doctest Rtree
+  doctest RTree
 
-  test "greets the world" do
-    assert Rtree.hello() == :world
+  describe "RTree" do
+    test "insert" do
+      node = %RNode{}
+      object = %RObject{x: 1, y: 1, data: "data"}
+
+      assert RTree.insert(node, object) == %RNode{
+               bounding: %RBoundingBox{min_x: 1, min_y: 1, max_x: 1, max_y: 1, area: 0},
+               children: nil,
+               objects: [%RObject{x: 1, y: 1, data: "data"}]
+             }
+    end
+
+    test "insert more than @m objects" do
+      node = %RNode{}
+      objects = for i <- 1..3, do: %RObject{x: i, y: i, data: "data"}
+      node = Enum.reduce(objects, node, fn object, acc -> RTree.insert(acc, object) end)
+
+      assert RTree.insert(node, %RObject{x: 4, y: 4, data: "data"}) == %RNode{
+               bounding: %RBoundingBox{min_x: 1, min_y: 1, max_x: 4, max_y: 4, area: 9},
+               children: %{
+                 left: %RNode{
+                   bounding: %RBoundingBox{min_x: 1, min_y: 1, max_x: 2, max_y: 2, area: 1},
+                   children: nil,
+                   objects: [
+                     %RObject{x: 1, y: 1, data: "data"},
+                     %RObject{x: 2, y: 2, data: "data"}
+                   ]
+                 },
+                 right: %RNode{
+                   bounding: %RBoundingBox{min_x: 3, min_y: 3, max_x: 4, max_y: 4, area: 1},
+                   children: nil,
+                   objects: [
+                     %RObject{x: 3, y: 3, data: "data"},
+                     %RObject{x: 4, y: 4, data: "data"}
+                   ]
+                 }
+               },
+               objects: []
+             }
+    end
   end
 end
